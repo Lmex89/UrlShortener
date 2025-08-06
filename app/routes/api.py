bd02d283edd424671f2206546a156d62b2c67cb8
luimex <bjt.mex@gmail.com>
2025-08-05 21:06:00 -0600

    deleting prefix

--- app/routes/api.py
+++ app/routes/api.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from routes import controller
 
 api_router = APIRouter()
-api_router.include_router(controller.router, tags=["/base"], prefix="/url")
+api_router.include_router(controller.router, tags=["/base"])
