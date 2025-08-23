5b91ce1f3e1996391711f59337dc9eb6e9b62a4b
luimex <bjt.mex@gmail.com>
2025-08-23 10:34:15 -0600

    adding logs

--- app/services/constants.py
+++ app/services/constants.py
@@ -4,5 +4,6 @@ from loguru import logger
 HOST_URL = os.getenv("HOST_URL")
 CREATE_API_KEY = os.getenv("CREATE_API_KEY")
 logger.info(f"create apikey {CREATE_API_KEY}")
-
+DELETE_API_KEY = os.getenv("DELETE_API_KEY")
+logger.info(f"create apikey {DELETE_API_KEY}")
 TIME_EXPIRATION_URL= 7
